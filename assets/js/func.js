f32d32edd24fcd1e883f98e11342fba3a96e18c8
charliesmart <charles.smart@uconn.edu>
2016-01-16 00:17:35 -0500

    Add skill bar labels, along with much needed js comments

--- assets/js/func.js
+++ assets/js/func.js
@@ -41,35 +41,42 @@ $(document).ready(function(){
     Set skills bars and text to proper values
     ---------------------------------------*/
 
+    // Cash these values
     var $window = $(window);
     var $skillBars = $('.skill-inner');
 
-    $(window).scroll();
+    //Trigger a scroll event when you load the page
+    $window.trigger('scroll');
 
+    //Main function that checks whether skill bars are on screen
     function animate_bar_on_scroll() {
-        var winHeight = $window.height();
-        var winTop = $window.scrollTop();
-        var winBottom = winHeight + winTop;
+        var winHeight = $window.height(); //Height of the window
+        var winTop = $window.scrollTop(); //How far you've scrolled to top of screen
+        var winBottom = winHeight + winTop; //How far you've scrolled to bottom of screen
 
+        //Check if each skill bar is visible on screen
         $.each($skillBars, function(){
-            var $bar = $(this);
-            var barHeight = $bar.outerHeight();
-            var barTop = $bar.offset().top;
-            var barBottom = barHeight + barTop;
+            var $bar = $(this); //Cash value
+            var barHeight = $bar.outerHeight(); //Get height of bar
+            var barTop = $bar.offset().top; //Get top offset of bar
+            var barBottom = barHeight + barTop; //Get bottom offset of bar
 
             //Check if element is in viewport
             if((barBottom >= winTop) && (barTop <= winBottom)) {
-                animate_bar($bar);
+                animate_bar($bar); //Call animate_bar function below
             } else {
-                $bar.css('width', 0);
-                $bar.children().attr('isSet', 'false');
+                $bar.css('width', 0); //Reset bar width
+                $bar.children().attr('isSet', 'false'); //Reset isSet - explained below
             }
         })
     }
 
+    //Function that actually does the animating once the bars are visible
     function animate_bar($bar) {
-        var percent = $bar.attr('value');
 
+        var percent = $bar.attr('value'); //Get skill percent from value attribut in HTML
+
+        //Set width of bar
         $bar.css('width', function(){
             var maxWidthText = $bar.css('maxWidth'); //Get maximum width of inner bar (comes in 'px' format)
             var maxWidthNum = parseInt(maxWidthText.replace('px', '')) //Convert previous string to useable numbers
@@ -77,24 +84,30 @@ $(document).ready(function(){
             return width.toString() + 'px'; //Return bar length as string with 'px'
         });
 
-        $bar.children().each(function(){
-            var $num = $(this);
-            var currentValue = $num.text();
+        //Set and animate percentage text
+        $bar.children().each(function(){ //Select children (p's containing percentage)
+
+            var $num = $(this); //Cash this
+
+            //We have to check is isSet is true before we execute. isSet becomes true when the percent increment animation
+            //first happens, and becomes false when the elements go off screen. This ensure the percent increment animation
+            //does not continuously fire as the user scrolls with the elements on screen.
             if ($num.attr('isSet') == 'false') {
-                $({numberValue: currentValue}).animate({numberValue: percent}, {
+                $({numberValue: 0}).animate({numberValue: percent}, { //Animate percent value from 0 to percent, defined earlier in animate_bar
                     duration: 800,
                     easing: 'swing',
                     step: function() {
-                        $num.text(Math.ceil(this.numberValue) + '%');
+                        $num.text(Math.ceil(this.numberValue) + '%'); //Actually set the text to the number with eprcent sign
                     },
                     done: function() {
                         $num.text(Math.ceil(this.numberValue) + '%');
                     }
                 });
-                $num.attr('isSet', 'true');
+                $num.attr('isSet', 'true'); //Set isSet to true once the animation is complete
             }
         });
     }
 
+    //Fire everything above whenever the user scrolls or resizes the window
     $window.on('scroll resize', animate_bar_on_scroll);
 });
