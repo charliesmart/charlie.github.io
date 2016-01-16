c490d56606a45b98589e3a895163af1a4e3fc92a
charliesmart <charles.smart@uconn.edu>
2016-01-15 20:11:33 -0500

    Update about page with image and content

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
+
+    /*--------------------------------------
+    Function to autosrcoll to point when user
+    clicks nav bar links.
+    ---------------------------------------*/
     $('.autoscroll').on('click', function(){
-        var id = $(this).attr('id');
+        var id = $(this).attr('id'); //Gets id of link
         $('html, body').animate({
-            scrollTop: $('.' + id).offset().top-60
+            scrollTop: $('.' + id).offset().top-60 //Finds div with class that matches link id and scrolls to it
         }, 500, "swing");
     });
 
-    var navTop = $('.nav-main').offset().top;
+
+    /*--------------------------------------
+    Function to create the 'sticky nav'
+    ---------------------------------------*/
+    var navTop = $('.nav-main').offset().top; //Finds offset of nav
+
+    $(window).resize(function(){
+        navTop = $('.nav-main').offset().top; //Resets navTop on window resize
+    });
+
     var stickyNav = function() {
         var scrollTop = $(window).scrollTop();
-        if(scrollTop > navTop) {
+        if(scrollTop > navTop) {                 //If scroll is greater than nav offset, make nav sticky
             $('.nav-main').addClass('sticky');
         } else {
             $('.nav-main').removeClass('sticky');
