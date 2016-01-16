954673175805bb40bb1102c793089bf3b78dc71f
charliesmart <charles.smart@uconn.edu>
2016-01-15 23:38:08 -0500

    Add function to animate percentages in skill bars

--- assets/js/func.js
+++ assets/js/func.js
@@ -40,15 +40,61 @@ $(document).ready(function(){
     /*--------------------------------------
     Set skills bars and text to proper values
     ---------------------------------------*/
-    $('.skill-inner').css('width', function(){
-        var percent = $(this).attr('value'); //Get percent value for bar
-        var maxWidthText = $(this).css('maxWidth'); //Get maximum width of inner bar (comes in 'px' format)
-        var maxWidthNum = parseInt(maxWidthText.replace('px', '')) //Convert previous string to useable numbers
-        var width = Math.round(percent / 100 * maxWidthNum); //Calculate final bar length
-        return width.toString() + 'px'; //Return bar length as string with 'px'
-    });
 
-    $('.skill-inner p').text(function(){
-        return $(this).parent().attr('value').toString() + '%';
-    });
+    var $window = $(window);
+    var $skillBars = $('.skill-inner');
+
+    $(window).scroll();
+
+    function animate_bar_on_scroll() {
+        var winHeight = $window.height();
+        var winTop = $window.scrollTop();
+        var winBottom = winHeight + winTop;
+
+        $.each($skillBars, function(){
+            var $bar = $(this);
+            var barHeight = $bar.outerHeight();
+            var barTop = $bar.offset().top;
+            var barBottom = barHeight + barTop;
+
+            //Check if element is in viewport
+            if((barBottom >= winTop) && (barTop <= winBottom)) {
+                animate_bar($bar);
+            } else {
+                $bar.css('width', 0);
+                $bar.children().attr('isSet', 'false');
+            }
+        })
+    }
+
+    function animate_bar($bar) {
+        var percent = $bar.attr('value');
+
+        $bar.css('width', function(){
+            var maxWidthText = $bar.css('maxWidth'); //Get maximum width of inner bar (comes in 'px' format)
+            var maxWidthNum = parseInt(maxWidthText.replace('px', '')) //Convert previous string to useable numbers
+            var width = Math.round(percent / 100 * maxWidthNum); //Calculate final bar length
+            return width.toString() + 'px'; //Return bar length as string with 'px'
+        });
+
+        $bar.children().each(function(){
+            var $num = $(this);
+            var currentValue = $num.text();
+            if ($num.attr('isSet') == 'false') {
+                $({numberValue: currentValue}).animate({numberValue: percent}, {
+                    duration: 800,
+                    easing: 'swing',
+                    step: function() {
+                        $num.text(Math.ceil(this.numberValue) + '%');
+                    },
+                    done: function() {
+                        $num.text(Math.ceil(this.numberValue) + '%');
+                    }
+                });
+                $num.attr('isSet', 'true');
+            }
+        });
+    }
+
+    $window.on('scroll resize', animate_bar_on_scroll);
 });
