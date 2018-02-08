46107a7f5fa959a78df2de33d35b225353ab2834
Charlie Smart <charlie.r.smart@gmail.com>
2018-02-07 22:19:30 -0500

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,9 @@
 $(document).ready(function(){
-    function resize_posts(){
-        $('.post-img').css('height', function(){
-            return $(this).width();
-        });
-    }
-
-    resize_posts();
-
-    $(window).resize(function(){
-        resize_posts();
-    });
-
-    $('#work-link').on('click', function(){
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('#work').offset().top
-        }, 200);
-    })
-
-
-    var $grid = $('#grid').isotope({
-        layoutMode: 'fitRows'
-    });
-
-    $('.filter').on('click', function(){
-        var filter_value = '.' + $(this).attr('id');
-        if (filter_value == '.all') {
-            $grid.isotope({ filter: '*' });
-        } else {
-            $grid.isotope({ filter: filter_value });
-        }
-    })
-
+  $('nav p').on('click', function() {
+    $('nav p').removeClass('selected');
+    $(this).addClass('selected');
+    
+    $('.show').removeClass('show');
+    $('.' + this.innerText).addClass('show');
+  })
 })
